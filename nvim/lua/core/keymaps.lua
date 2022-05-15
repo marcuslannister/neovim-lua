0dbc55ed45ef5b81a93ba01b2fae7acb053e4e61
marcuslannister <marcus@lannister.cc>
2022-05-15 10:06:01 +0800

    feat: add comment plugin kommentary

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -64,3 +64,8 @@ map('n', '<leader>n', ':NvimTreeFindFile<CR>')      -- search file
 
 -- Tagbar
 map('n', '<leader>z', ':TagbarToggle<CR>')          -- open/close
+
+-- kommentary
+vim.api.nvim_set_keymap("n", "<leader>cc", "<Plug>kommentary_line_default", {})
+vim.api.nvim_set_keymap("n", "<leader>c", "<Plug>kommentary_motion_default", {})
+vim.api.nvim_set_keymap("x", "<leader>c", "<Plug>kommentary_visual_default", {})
