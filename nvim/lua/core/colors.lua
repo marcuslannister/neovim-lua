bab8f4417711766af683fca875b5ad503545d8cb
marcuslannister <marcus@lannister.cc>
2022-09-05 20:38:17 +0800

    feat: disable italic style

--- nvim/lua/core/colors.lua
+++ nvim/lua/core/colors.lua
@@ -48,6 +48,36 @@ require("catppuccin").setup({
   catppuccin_flavour = "latte", -- latte, frappe, macchiato, mocha
   -- catppuccin_flavour = "frappe", -- latte, frappe, macchiato, mocha
   -- other config
+	transparent_background = false,
+	term_colors = false,
+	compile = {
+		enabled = false,
+		path = vim.fn.stdpath("cache") .. "/catppuccin",
+	},
+	dim_inactive = {
+		enabled = false,
+		shade = "dark",
+		percentage = 0.15,
+	},
+	styles = {
+		comments = {},
+		conditionals = {},
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	integrations = {
+		-- For various plugins integrations see https://github.com/catppuccin/nvim#integrations
+	},
+	color_overrides = {},
+	highlight_overrides = {},
 })
 vim.cmd [[colorscheme catppuccin]]
 
