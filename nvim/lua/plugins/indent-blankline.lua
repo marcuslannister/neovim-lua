0e5707efe3a663e4d586c1a4f5801d534ab0d421
marcuslannister <marcus@lannister.cc>
2022-12-09 16:45:41 +0800

    feat: add tab/trail char and show context start

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -11,10 +11,20 @@ if not status_ok then
   return
 end
 
+vim.opt.list = true
+vim.opt.listchars:append "tab:>-"
+vim.opt.listchars:append "trail:-"
+
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "eol:↴"
+
 indent_blankline.setup {
   char = "▏",
   use_treesitter = true,
   show_first_indent_level = true,
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
   filetype_exclude = {
     'lspinfo',
     'packer',
