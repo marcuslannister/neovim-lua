841db101dde843402c642ff3b83f5173ebb39e73
marcuslannister <marcus@lannister.cc>
2022-12-09 15:01:07 +0800

    feat: add incremental selection, indent, folding

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -24,4 +24,26 @@ nvim_treesitter.setup {
     -- `false` will disable the whole extension
     enable = true,
   },
+  -- 启用增量选择模块
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      node_decremental = "<BS>",
+      scope_incremental = "<TAB>",
+    },
+  },
+  -- 启用代码缩进模块 (=)
+  indent = {
+    enable = true,
+  },
 }
+
+-- 开启 Folding 模块
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- 默认不要折叠
+-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+vim.opt.foldlevel = 99
+
