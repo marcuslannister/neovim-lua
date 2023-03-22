eca7c0dbac76e1c61249c58d757d9be51d1ae478
marcuslannister <marcus@lannister.cc>
2023-03-22 19:49:33 +0800

    feat: Enable Org mode support in Treesitter

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -10,22 +10,11 @@
 -- Load custom treesitter grammar for org filetype
 require('orgmode').setup_ts_grammar()
 
--- Treesitter configuration
-require('nvim-treesitter.configs').setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-  -- highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    -- Required for spellcheck, some LaTex highlights and
-    -- code block highlights that do not have ts grammar
-    additional_vim_regex_highlighting = {'org'},
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
-}
-
 require('orgmode').setup({
-  org_agenda_files = {'~/orgmode/**/*'},
-  org_default_notes_file = '~/orgmode/refile.org',
+  org_agenda_files = {'~/org/**/*'},
+  org_default_notes_file = '~/org/refile.org',
+  win_split_mode = 'float',
+  org_todo_keywords = {'TODO(t)', 'NEXT(n)', '|', 'DONE(d)'},
 })
 
 
